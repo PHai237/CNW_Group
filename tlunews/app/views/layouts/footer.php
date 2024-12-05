a78feace1f9d672aa1205ef8c53eb0e05b5110f9
Magica <Stone23072004@gmail.com>
2024-12-05 23:35:03 +0700

    created a big folder named app with public folder

---
+++ tlunews/app/views/layouts/footer.php
@@ -0,0 +1,25 @@
+<footer style="background-color: #FFFFFF; width: 100%; padding: 20px; border-top: 5px solid #eaeaea;">
+  <div style="max-width: 1200px; margin: 0 auto; display: flex; justify-content: space-between; align-items: center; font-family: 'Roboto', sans-serif; font-size: 16px; color: #333;">
+    <div style="width: 100%;">
+      <div class="footer-top" style="display: flex; align-items: center;">
+        <div class="footer-logo">
+          <img src="https://cdn.pixabay.com/photo/2016/09/04/17/46/news-1644696_1280.png" style="width: 150px; height: 100px;" alt="logo">
+        </div>
+        <div class="footer-title " style="width:100%">
+          <h3 style="margin: 0; font-size: 22px; font-weight: bold; margin-left: 50%; color: #007bff;">TRANG BÁO TIN TỨC THẾ GIỚI 24H MỚI NHẤT</h3>
+        </div>
+      </div>
+      <div class="info-container" style="margin-top: 10px; padding: 20px; background-color: #f9f9f9; border-radius: 5px;">
+        <div class="right-info" style="text-align: right;">
+        <p>
+            Báo tiếng Việt nhiều người xem nhất<br>
+            Thuộc Bộ Khoa học và Công nghệ<br>
+            Số giấy phép: 548/GP-BTTTT<br>
+            Do Bộ Thông tin và Truyền thông cấp<br>
+            ngày 24/08/2021
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
